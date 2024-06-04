f7b21b3f33e4421a906e87c22d6de1a65c6b17ea
Raul Gil <raul.emmanuel.gil@gmail.com>
2024-06-04 03:47:33 -0500

    include schemas in __init__.py for easier imports

---
+++ app/schemas/__init__.py
@@ -0,0 +1,23 @@
+# noqa
+from app.schemas.location_schema import (
+    LocationResponseSchema,
+    LocationCreateRequestSchema,
+    LocationUpdateRequestSchema,
+)
+from app.schemas.sprocket_type_schema import (
+    SprocketTypeResponseSchema,
+    SprocketTypeCreateRequestSchema,
+    SprocketTypeUpdateRequestSchema,
+)
+from app.schemas.factory_history_schema import FactoryHistoryResponseSchema
+from app.schemas.factory_schema import (
+    FactoryResponseSchema,
+    FactoryCreateRequestSchema,
+    FactoryUpdateRequestSchema,
+    FactoryResponseSchemaWithChartData,
+)
+from app.schemas.sprocket_schema import (
+    SprocketResponseSchema,
+    SprocketCreateRequestSchema,
+    SprocketUpdateRequestSchema,
+)
