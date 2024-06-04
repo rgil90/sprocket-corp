6b35b0e5fca13f2744a1d94cb52bd44f30c174a1
Raul Gil <raul.emmanuel.gil@gmail.com>
2024-06-04 03:47:21 -0500

    include models in __init__.py for easier migration detection

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,6 @@
-from app.db_connection import Base
-from app.models.location import Location
+from app.db_connection import Base  # noqa: F401
+from app.models.location_model import Location
+from app.models.sprocket_type_model import SprocketType
+from app.models.factory_history_model import FactoryHistory
+from app.models.factory_model import Factory
+from app.models.sprocket_model import Sprocket
