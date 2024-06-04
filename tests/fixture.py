0ea1f942d6e48ee8d6b8bfd92335510855f9edce
Raul Gil <raul.emmanuel.gil@gmail.com>
2024-06-03 20:55:46 -0500

    add db_session fixture

---
+++ tests/fixture.py
@@ -0,0 +1,13 @@
+import os
+
+import pytest
+from sqlalchemy import create_engine
+
+from tests.utils.database_utils import migrate_to_db
+
+
+@pytest.fixture(scope='session', autouse=True)
+def db_session():
+    engine = create_engine(os.environ.get("TEST_DATABASE_URL"))
+    with engine.begin() as connection:
+        migrate_to_db("app/migrations/", "app/migrations/alembic.ini", connection)
